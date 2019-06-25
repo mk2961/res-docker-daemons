f24315a89112b03c5d031c1a437b152e2c3c7196
Tony Sabal <tony.sabal@gmail.com>
2019-06-25 10:55:37 -0700

    Initial Commit

---
+++ src/main/java/com/docker_daemons/DockerDaemonsApplication.java
@@ -0,0 +1,13 @@
+package com.docker_daemons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDaemonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDaemonsApplication.class, args);
+	}
+
+}
